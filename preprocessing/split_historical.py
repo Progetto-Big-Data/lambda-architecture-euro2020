79ca4f1f482efdf44c7be202fa459581588485e0
Antonio Turco <antonioturco@email.com>
2021-07-01 18:34:39 +0200

    :monocle_face: removes recent fixtures from the historical dataset

---
+++ preprocessing/split_historical.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help='insert absolute path to the historical file', type=str)
+args = parser.parse_args()
+
+filename = args.filename
+
+historical_fixtures = pd.read_csv(filename)
+
+euro2020 = {
+    'start_date' : '2021-06-11',
+    'competition_name' : 'UEFA Euro'
+}
+
+recent_fixtures = historical_fixtures[historical_fixtures.date >= euro2020['start_date']]
+old_fixtures = historical_fixtures[historical_fixtures.date < euro2020['start_date']]
+
+recent_european_fixtures = recent_fixtures[recent_fixtures.tournament == euro2020['competition_name']]
+old_european_fixtures = old_fixtures[old_fixtures.tournament == euro2020['competition_name']]
+
+recent_european_fixtures.to_csv('recent_fixtures.csv', index=False)
+old_european_fixtures.to_csv('old_fixtures.csv', index=False)
