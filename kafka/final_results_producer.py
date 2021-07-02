c64d23d28078696d8bfb658d57335df4328ae8db
Antonio Turco <antonioturco@email.com>
2021-07-02 13:46:48 +0200

    :construction: adds a basic csv producer from the recent fixtures

---
+++ kafka/final_results_producer.py
@@ -0,0 +1,21 @@
+from os import sep
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import csv 
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+fixture_path = 'recent_fixtures.csv'
+kafka_topic = 'final_fixtures'
+
+with open(fixture_path, 'r') as csvfile:
+    fixtures = csv.reader(csvfile)
+    for game in fixtures:
+        game = ",".join(game)
+        producer.send(kafka_topic, value=game)
+        print(f'sent data {game}')
+        sleep(5)
