dbf51f43c25ed446ac4d8f2d9634f24adb9916fd
Antonio Turco <antonioturco@email.com>
2021-07-03 17:08:46 +0200

    :construction: adds simple stats extractor

---
+++ dataset/statistics_extractor.py
@@ -0,0 +1,18 @@
+import http.client
+
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': ""
+    }
+
+euro2020_id = 4
+fixture = 718186
+
+conn.request("GET", f"/fixtures/statistics?fixture={fixture}", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
