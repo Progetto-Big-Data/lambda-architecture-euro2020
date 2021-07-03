a8a26ba62a99f6731233c46a370c43f79ae3c1cc
Antonio Turco <antonioturco@email.com>
2021-07-03 17:26:04 +0200

    :sparkles: adds polling to the stats extractor

--- dataset/statistics_extractor.py
+++ dataset/statistics_extractor.py
@@ -1,18 +1,32 @@
 import http.client
+import time
 
 conn = http.client.HTTPSConnection("v3.football.api-sports.io")
 
 headers = {
     'x-rapidapi-host': "v3.football.api-sports.io",
     'x-rapidapi-key': ""
-    }
+}
 
 euro2020_id = 4
 fixture = 718186
 
-conn.request("GET", f"/fixtures/statistics?fixture={fixture}", headers=headers)
+polling_time = 60 #seconds
 
-res = conn.getresponse()
-data = res.read()
+minute = 1
 
-print(data.decode("utf-8"))
+
+while True:
+    print(f"minute {minute}")
+    conn.request("GET", f"/fixtures/statistics?fixture={fixture}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    f = open(f"match_minute_{minute}.json", "w")
+    f.write(data.decode("utf-8"))
+    f.close()
+
+    minute += 1
+    time.sleep(polling_time)
+    
