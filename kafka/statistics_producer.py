d91e30dca5a7c38e0e71ff4703ec41fde3f4ccd2
Antonio Turco <antonioturco@email.com>
2021-07-11 11:40:59 +0200

    :sparkles: adds statistics kafka producer

---
+++ kafka/statistics_producer.py
@@ -0,0 +1,21 @@
+import json
+from os import sep
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import csv 
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+fixture_path = 'dataset/delta_filled_na_stats_718186.json'
+kafka_topic = 'fixture_718186'
+
+with open(fixture_path, 'r') as file:
+    fixture = json.load(file)
+    for minute in fixture:
+        producer.send(kafka_topic, value=minute)
+        print(f'sent data {minute}')
+        sleep(5)
