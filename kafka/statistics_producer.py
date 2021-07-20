b3eafb77d6b722ffdb4f93a97a95bc4032f7fc75
Alex <alexandru.pavel.94@gmail.com>
2021-07-20 02:00:41 +0200

    parametrized topic creation (still junk code)

--- kafka/statistics_producer.py
+++ kafka/statistics_producer.py
@@ -6,21 +6,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--interval', help='Period of time between two messages', type=float)
+parser.add_argument('--fixture', help='Fixture id', type=str)
 
 args = parser.parse_args()
 
 period_time = 1
+fixture = 718186
 
 if args.interval:
     period_time = args.interval
+
+if args.fixture:
+    fixture = args.fixture
 
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda x: dumps(x).encode('utf-8')
 )
 
-fixture_path = 'dataset/delta_filled_na_stats_718186.json'
-kafka_topic = 'fixture_718186'
+fixture_path = f'dataset/delta_filled_na_stats_{fixture}.json'
+kafka_topic = f'fixture_{fixture}'
 
 with open(fixture_path, 'r') as file:
     fixture = json.load(file)
