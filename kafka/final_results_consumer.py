a62b0ff5757d213a7abfbfb33d52b6257c654edd
Antonio Turco <antonioturco@email.com>
2021-07-23 16:14:39 +0200

    :recycle: removes testing code

--- kafka/final_results_consumer.py
+++ kafka/final_results_consumer.py
@@ -7,16 +7,17 @@ import os
 
 
 class StoreHDFS(threading.Thread):
-    def __init__(self, hdfs_filepath, filename, sleep_time=5):
+    def __init__(self, hdfs_filepath, filename, local_path, sleep_time=5):
         threading.Thread.__init__(self)
         self.hdfs_path = hdfs_filepath
         self.sleep_time = sleep_time
         self.filename = filename
+        self.local_path = local_path
 
     def run(self):
         time.sleep(self.sleep_time)
         filepath = os.path.join(self.hdfs_path, self.filename)
-        hdfs_command = f'hdfs dfs -put test.csv {filepath}'
+        hdfs_command = f'hdfs dfs -put {self.local_path} {filepath}'
         os.system(hdfs_command)
         print('file saved to HDFS')
             
@@ -29,12 +30,16 @@ consumer = kafka.KafkaConsumer(
     bootstrap_servers=['localhost:9092']
 )
 
-hdfs_path = '/user/antonio/historical_euro_fixtures'
 
-hdfs_write_daemon = StoreHDFS(hdfs_path, 'test.csv')
+local_path = 'kafka'
+filename = 'new_fixtures.csv'
+local_path = os.path.join(local_path, filename)
+hdfs_path = 'historical_euro_fixtures'
+
+hdfs_write_daemon = StoreHDFS(hdfs_path, filename, local_path)
 hdfs_write_daemon.start()
 
-with open('test.csv', 'a') as file:
+with open(local_path, 'a') as file:
     for msg in consumer:
         fixture = msg.value
         fixture = bytes.decode(fixture)
